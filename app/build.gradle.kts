3e4dc053718cf7a49cc2432b957d164acac2e09e
akashnavodyaperera <akashnavodya4582@gmail.com>
2026-01-12 22:35:37 +0530

    Making Parentdashboard

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,11 @@
+// CORRECT
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.compose) // You likely need this based on your toml
     id("com.google.gms.google-services")
 }
+
 
 android {
     namespace = "com.wycherley.trackmybus"
