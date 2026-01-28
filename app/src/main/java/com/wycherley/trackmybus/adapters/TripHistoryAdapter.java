553dd27eb37731a07dfde5a3193712ebaffc4ce4
akashnavodyaperera <akashnavodya4582@gmail.com>
2026-01-28 15:39:27 +0530

    TripHistoryAdapter.java

--- app/src/main/java/com/wycherley/trackmybus/adapters/TripHistoryAdapter.java
+++ app/src/main/java/com/wycherley/trackmybus/adapters/TripHistoryAdapter.java
@@ -1,4 +1,36 @@
 package com.wycherley.trackmybus.adapters;
 
-public class TripHistoryAdapter {
-}
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.wycherley.trackmybus.R;
+import com.wycherley.trackmybus.models.TripHistory;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TripHistoryAdapter extends RecyclerView.Adapter<TripHistoryAdapter.ViewHolder> {
+    private List<TripHistory> trips;
+    private OnTripClickListener clickListener;
+
+    public interface OnTripClickListener {
+        void onTripClick(TripHistory trip);
+    }
+
+    public TripHistoryAdapter() {
+        this.trips = new ArrayList<>();
+    }
+
+    public void setTrips(List<TripHistory> trips) {
+        this.trips = trips;
+        notifyDataSetChanged();
+    }
+
+    public void setOnTripClickListener(OnTripClickListener listener) {
+        this.clickListener = listener;
+    }
+
